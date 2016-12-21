fd75f00ad8ce27f151b891dee99ce08676dae0b0
soonfy <soonfy@163.com>
2016-12-21 12:59:59 +0800

    run

--- crawl_douban/run.py
+++ crawl_douban/run.py
@@ -12,25 +12,31 @@ from spider_middleware.ua import write_ua
 from douban_user.user_starter import run as run_user
 from douban_movie.user_movie_starter import run as run_user_movie
 
-print('spider ready, go...')
-userid = '67492098'
+print('spider ready, go...\r\n')
+userid = input('input douban userid --> ')
+print('you want to crawl douban user %s' % userid)
+print('open > https://www.douban.com/people/%s/ < to view data...\r\n' % userid)
+print('==> 10s start crawl...\r\n')
+time.sleep(10)
 
-print('firstly, update ua...')
-write_ua()
-print('rest 1 min...')
+print('==> firstly, update ua...')
+# write_ua()
+# print('==> rest 10s...\r\n')
+# time.sleep(10)
+
+# print('==> secondly, login and crawl user contacts...')
+# run_user(userid)
+# print('==> all user contacts over...')
+# print('==> rest 1 min...\r\n')
+# time.sleep(60)
+
+# print('==> thirdly, crawl user movies...')
+# userfile = ('./crawl_douban/douban_user/users.txt')
+# userids = open(userfile).read().split()
+# for userid in userids:
+#   run_user_movie(userid)
+# print('==> all users movies saved...')
+print('==> rest 1 min...\r\n')
 time.sleep(60)
 
-print('secondly, login and crawl user contacts...')
-run_user(userid)
-print('all user contacts over...')
-print('rest 1 min...')
-time.sleep(60)
-
-print('thirdly, crawl user movies...')
-userfile = ('./crawl_douban/douban_user/users.txt')
-userids = open(userfile).read().split()
-for userid in userids:
-  run_user_movie(userid)
-print('all users movies saved...')
-print('rest 1 min...')
-time.sleep(60)
+print('spider over...\r\n')
